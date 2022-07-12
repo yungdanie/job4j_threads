59c2ee25ef1fb035801d21fb038c2e1d98c5038b
yungdanie <nikoches253@gmail.com>
2022-07-12 19:46:44 +0300

    4. Скачивание файла с ограничением. [#144271 #321137]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 import java.net.URL;
 
 public class Wget implements Runnable {
+
+    public static final long ONE_SEC = 1000;
+    public static final int DEFAULT_BUFFER_SIZE = 1024;
     private final String url;
     private final int speed;
 
@@ -17,25 +20,42 @@ public class Wget implements Runnable {
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("loaded.xml")) {
-            Long startTime = System.currentTimeMillis();
-            byte[] dataBuffer = new byte[speed];
+             FileOutputStream fileOutputStream = new FileOutputStream("loaded.dat")) {
+            byte[] dataBuffer = new byte[DEFAULT_BUFFER_SIZE];
             int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
-                Long endTime = System.currentTimeMillis() - startTime;
-                if (endTime < 1000) {
-                    Thread.sleep(endTime);
+            long bytesWrited = 0;
+            long endTime;
+            long startTime = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, DEFAULT_BUFFER_SIZE)) != -1) {
+                bytesWrited = bytesWrited + bytesRead;
+                if (bytesWrited >= speed) {
+                    endTime = System.currentTimeMillis() - startTime;
+                    if (endTime < ONE_SEC) {
+                        bytesWrited = 0;
+                        Thread.sleep(ONE_SEC - endTime);
+                    }
                 }
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
             }
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static void validate(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Illegal number of arguments."
+                    .concat(System.lineSeparator())
+                    .concat("Put URL of the file which you want to download as first argument")
+                    .concat(System.lineSeparator())
+                    .concat("Put download speed (byte/sec) as second argument"));
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
+        validate(args);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new Wget(url, speed));
