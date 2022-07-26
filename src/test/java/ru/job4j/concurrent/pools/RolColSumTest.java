92e8403f2c1ec25373cf10a3c2c4cab50c95380a
yungdanie <nikoches253@gmail.com>
2022-07-26 14:22:12 +0300

    4. CompletableFuture [#361626 #325721]

--- src/test/java/ru/job4j/concurrent/pools/RolColSumTest.java
+++ src/test/java/ru/job4j/concurrent/pools/RolColSumTest.java
@@ -5,29 +5,34 @@ import org.junit.jupiter.api.Test;
 
 class RolColSumTest {
 
-    private final static RolColSum.Sums FIRST = new RolColSum.Sums();
-    private final static RolColSum.Sums SECOND = new RolColSum.Sums();
-    private final static RolColSum.Sums THIRD = new RolColSum.Sums();
-
-    private final static RolColSum.Sums[] EXPECTED = new RolColSum.Sums[] {FIRST, SECOND, THIRD};
-
-    static {
+    @Test
+    void whenLinearSum() {
+        RolColSum.Sums FIRST = new RolColSum.Sums();
+        RolColSum.Sums SECOND = new RolColSum.Sums();
+        RolColSum.Sums THIRD = new RolColSum.Sums();
         FIRST.setRowSum(6);
         FIRST.setColSum(10);
         SECOND.setRowSum(12);
         SECOND.setColSum(13);
         THIRD.setRowSum(21);
         THIRD.setColSum(16);
-    }
-
-    @Test
-    void whenLinearSum() {
+        RolColSum.Sums[] EXPECTED = new RolColSum.Sums[] {FIRST, SECOND, THIRD};
         RolColSum.Sums[] result = RolColSum.sum(new int[][] {{1, 2, 3}, {3, 4, 5}, {6, 7, 8}});
         Assertions.assertThat(result).isEqualTo(EXPECTED);
     }
 
     @Test
     void whenAsyncSum() {
+        RolColSum.Sums FIRST = new RolColSum.Sums();
+        RolColSum.Sums SECOND = new RolColSum.Sums();
+        RolColSum.Sums THIRD = new RolColSum.Sums();
+        FIRST.setRowSum(6);
+        FIRST.setColSum(10);
+        SECOND.setRowSum(12);
+        SECOND.setColSum(13);
+        THIRD.setRowSum(21);
+        THIRD.setColSum(16);
+        RolColSum.Sums[] EXPECTED = new RolColSum.Sums[] {FIRST, SECOND, THIRD};
         RolColSum.Sums[] result = RolColSum.asyncSum(new int[][] {{1, 2, 3}, {3, 4, 5}, {6, 7, 8}});
         Assertions.assertThat(result).isEqualTo(EXPECTED);
     }
